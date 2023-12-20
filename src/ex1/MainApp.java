8d560f4085ba1ab0a2a427d402645a2a4a0e119a
xlRexpect <dragosflorea02@yahoo.com>
2023-12-20 18:32:33 +0200

    lab3

--- src/ex1/MainApp.java
+++ src/ex1/MainApp.java
@@ -1,2 +1,224 @@
-package ex1;public class MainApp {
+package ex1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class MainApp {
+    public static void main(String[] args) throws IOException {
+        class Parabola{
+            private int a,b,c;
+
+            public Parabola(int a,int b,int c){
+                this.a=a;
+                this.b=b;
+                this.c=c;
+
+            }
+
+            @Override
+            public String toString() {
+                return a+"x^2+"+b+"x+"+c;
+            }
+
+            public String VarfPar(){
+                float vx1,vy1,vx2,vy2;
+                if(a!=0) {
+                    vx1 =-b;
+                    vx1=vx1/(2*a);
+                    vy1=-(b*b);
+                    vy1=vy1+4*a*c;
+                    vy1=vy1/(4*a);
+                    //System.out.println(vx1+" "+vy1);
+                    return "("+vx1+" , "+vy1+")";
+                }else{
+                    return "error";
+                }
+            }
+            public String calcMijlDrept(int k,int l,int m){
+                float vx1,vy1,vx2,vy2;
+                if(a!=0) {
+                    vx1 =-b;
+                    vx1=vx1/(2*a);
+                    vy1=-(b*b);
+                    vy1=vy1+4*a*c;
+                    vy1=vy1/(4*a);
+                    //System.out.println(vx1+" "+vy1);
+                }else{
+                    return "error";
+                }
+                if(k!=0) {
+                    vx2 =-l;
+                    vx2=vx2/(2*k);
+                    vy2=-(l*l);
+                    vy2=vy2+4*k*l;
+                    vy2=vy2/(4*k);
+                    //System.out.println(vx2+" "+vy2);
+                }else{
+                    return "error";
+                }
+
+                float rx,ry;
+                rx=(vx1+vx2)/2;
+                ry=(vy1+vy2)/2;
+                return "( " + rx+" , "+ry+" )";
+            }
+
+            public static String calcMijlDreptStat(int d,int e,int f,int k,int l,int m){
+                float vx1,vy1,vx2,vy2;
+                if(d!=0) {
+                    vx1 =-e;
+                    vx1=vx1/(2*d);
+                    vy1=-(e*e);
+                    vy1=vy1+4*d*f;
+                    vy1=vy1/(4*d);
+                    //System.out.println(vx1+" "+vy1);
+                }else{
+                    return "error";
+                }
+                if(k!=0) {
+                    vx2 =-l;
+                    vx2=vx2/(2*k);
+                    vy2=-(l*l);
+                    vy2=vy2+4*k*m;
+                    vy2=vy2/(4*k);
+                    //System.out.println(vx2+" "+vy2);
+                }else{
+                    return "error";
+                }
+
+                float rx,ry;
+                rx=(vx1+vx2)/2;
+                ry=(vy1+vy2)/2;
+                return "( " + rx+" , "+ry+" )";
+            }
+
+            public double calcLung(int k,int l,int m){
+                double vx1,vy1,vx2,vy2;
+                if(a!=0) {
+                    vx1 =-b;
+                    vx1=vx1/(2*a);
+                    vy1=-(b*b);
+                    vy1=vy1+4*a*c;
+                    vy1=vy1/(4*a);
+                    //System.out.println(vx1+" "+vy1);
+                }else{
+                    return 0;
+                }
+                if(k!=0) {
+                    vx2 =-l;
+                    vx2=vx2/(2*k);
+                    vy2=-(l*l);
+                    vy2=vy2+4*k*l;
+                    vy2=vy2/(4*k);
+                    //System.out.println(vx2+" "+vy2);
+                }else{
+                    return 0;
+                }
+                return Math.hypot((vx2-vx1),(vy2-vy1));
+            }
+
+            public static double calcLungStat(int a,int b,int c,int k,int l,int m){
+                double vx1,vy1,vx2,vy2;
+                if(a!=0) {
+                    vx1 =-b;
+                    vx1=vx1/(2*a);
+                    vy1=-(b*b);
+                    vy1=vy1+4*a*c;
+                    vy1=vy1/(4*a);
+                    //System.out.println(vx1+" "+vy1);
+                }else{
+                    return 0;
+                }
+                if(k!=0) {
+                    vx2 =-l;
+                    vx2=vx2/(2*k);
+                    vy2=-(l*l);
+                    vy2=vy2+4*k*l;
+                    vy2=vy2/(4*k);
+                    //System.out.println(vx2+" "+vy2);
+                }else{
+                    return 0;
+                }
+                return Math.hypot((vx2-vx1),(vy2-vy1));
+            }
+
+
+            public int getA() {
+                return a;
+            }
+            public void setA(int a) {
+                this.a = a;
+            }
+            public int getB() {
+                return b;
+            }
+            public void setB(int b) {
+                this.b = b;
+            }
+            public int getC() {
+                return c;
+            }
+            public void setC(int c) {
+                this.c = c;
+            }
+
+
+        }
+
+        BufferedReader flux_in0 = new BufferedReader(new FileReader("in.txt"));
+        String linie;
+        int n=0;
+        int a,b,c;
+        List<Parabola> p =new ArrayList<Parabola>();
+        while ((linie = flux_in0.readLine()) != null) {
+            n++;
+        }
+        flux_in0.close();
+        BufferedReader flux_in = new BufferedReader(new FileReader("in.txt"));
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(flux_in.readLine());
+            //while (st.hasMoreTokens()) {
+            //System.out.println(st.nextToken());
+
+            //}
+            a=Integer.parseInt(st.nextToken());
+            b=Integer.parseInt(st.nextToken());
+            c=Integer.parseInt(st.nextToken());
+            Parabola par =new Parabola(a,b,c);
+            //System.out.println(par);
+            p.add(par);
+        }
+
+
+        for(int i=0;i<n;i++){
+            System.out.println(p.get(i));
+            Parabola par=p.get(i);
+            System.out.println(par.VarfPar());
+        }
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("dati un index");
+        int j=sc.nextInt();
+        System.out.println("dati un alt index");
+        int k=sc.nextInt();
+        int d,e,f,g,h,i;
+        d=(p.get(j)).getA();
+        e=(p.get(j)).getB();
+        f=(p.get(j)).getC();
+        g=(p.get(k)).getA();
+        h=(p.get(k)).getB();
+        i=(p.get(k)).getC();
+        System.out.println(Parabola.calcMijlDreptStat(d,e,f,g,h,i));
+        System.out.println(Parabola.calcLungStat(d,e,f,g,h,i));
+
+
+    }
 }
+
--- src/ex2/MainApp.java
+++ src/ex2/MainApp.java
@@ -1,2 +1,189 @@
-package ex2;public class MainApp {
+package ex2;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class MainApp {
+    public static void main(String[] args) throws IOException {
+        class Produs{
+            private String Denumire;
+            private float Pret;
+            private int Cant;
+            private LocalDate DataExp;
+
+            private static float Profit;
+
+            public static float getProfit() {
+                return Profit;
+            }
+
+            public static void setProfit(float profit) {
+                Profit = profit;
+            }
+
+            Produs(String d, float p, int c, LocalDate de){
+                Denumire=d;
+                Pret=p;
+                Cant=c;
+                DataExp=de;
+            }
+
+            @Override
+            public String toString() {
+                return "Produs{" +
+                        "Denumire='" + Denumire + '\'' +
+                        ", Pret=" + Pret +
+                        ", Cant=" + Cant +
+                        ", DataExp=" + DataExp +
+                        '}';
+            }
+
+            public String getDenumire() {
+                return Denumire;
+            }
+
+            public void setDenumire(String denumire) {
+                Denumire = denumire;
+            }
+
+            public float getPret() {
+                return Pret;
+            }
+
+            public void setPret(float pret) {
+                Pret = pret;
+            }
+
+            public int getCant() {
+                return Cant;
+            }
+
+            public void setCant(int cant) {
+                Cant = cant;
+            }
+
+            public LocalDate getDataExp() {
+                return DataExp;
+            }
+
+            public void setDataExp(LocalDate dataExp) {
+                DataExp = dataExp;
+            }
+        }
+
+
+        List<Produs> listProdus= new ArrayList<Produs>();
+        BufferedReader flux_in0=new BufferedReader(new FileReader("produse.csv"));
+        String linie;
+        String den;
+        float pre;
+        int can;
+        LocalDate dat;
+        int n=0;
+        while((linie=flux_in0.readLine())!=null){
+            n++;
+        }
+
+        flux_in0.close();
+        BufferedReader flux_in=new BufferedReader(new FileReader("produse.csv"));
+        for(int i=0;i<n;i++){
+            linie=flux_in.readLine();
+            StringTokenizer st=new StringTokenizer(linie);
+            den=st.nextToken();
+            pre=Float.valueOf(st.nextToken());
+            can=Integer.valueOf(st.nextToken());
+            //can=Integer.parseInt(st.nextToken().toString());
+            dat=LocalDate.parse(st.nextToken());
+            Produs pro=new Produs(den,pre,can,dat);
+            listProdus.add(pro);
+        }
+
+        /*for(int i=0;i<n;i++){
+            System.out.println(listProdus.get(i));
+        }*/
+
+
+        byte ui;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Welcome!");
+        do{
+            System.out.println("Profit= "+Produs.getProfit());
+            System.out.println("0 = exit");
+            System.out.println("1 = afisare toate produsele");
+            System.out.println("2 = afisare produse expirate");
+            System.out.println("3 = vanzare produs");
+            System.out.println("4 = produsul cu pretul minim");
+            System.out.println("5 = produse cu pret mai mic decat");
+
+            ui=scan.nextByte();
+            switch (ui){
+                case 0:
+                    System.out.println("closing program");
+                    break;
+                case 1:
+                    for(int i=0;i<n;i++){
+                        System.out.println(listProdus.get(i));
+                    }
+                    break;
+                case 2:
+                    for(int i=0;i<n;i++) {
+                        LocalDate dt1 = LocalDate.now();
+                        LocalDate dt2 = listProdus.get(i).getDataExp();
+                        if (dt1.isAfter(dt2)) {
+                            System.out.println(listProdus.get(i));
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("ce produs a fost cumparat?");
+                    den=scan.next();
+                    for(int i=0;i<n;i++){
+                        can=listProdus.get(i).getCant();
+                        System.out.println(den+" ??? "+listProdus.get(i).getDenumire()+" , "+can);
+                        if(den.equals(listProdus.get(i).getDenumire()) && can>0){
+                            System.out.println("produs vandut!");
+                            listProdus.get(i).setCant(can-1);
+                            pre=listProdus.get(i).getPret();
+                            Produs.setProfit(Produs.getProfit()+pre);
+                        }
+                    }
+                    break;
+                case 4:
+                    float min=Float.MAX_VALUE;
+                    byte minInd=0;
+                    for(byte i=0;i<n;i++) {
+                        if (listProdus.get(i).getPret()<min){
+                            min=listProdus.get(i).getPret();
+                            //minInd=i;
+                        }
+                    }
+                    System.out.println(min);
+                    for(byte i=0;i<n;i++){
+                        if (listProdus.get(i).getPret()==min){
+                            System.out.println(listProdus.get(i));
+                        }
+                    }
+                    //System.out.println(listProdus.get(minInd));
+                    break;
+                case 5:
+                    PrintStream flux_out = new PrintStream ("out.csv");
+                    System.out.println("Introduceti pretul");
+                    min=scan.nextFloat();
+                    for(byte i=0;i<n;i++) {
+                        if (listProdus.get(i).getPret()<min){
+                            System.out.println(listProdus.get(i));
+                            flux_out.println(listProdus.get(i));
+                        }
+                    }
+                    flux_out.close();
+                    break;
+                default:
+                    System.out.println("error");
+            }
+        }while(ui!=0);
+
+
+    }
 }
